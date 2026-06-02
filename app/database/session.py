01c45062cf6e25642378204698281c66b83718fd
Theekshana Nuwan <theekshanabro40@gmail.com>
2026-06-02 11:45:05 +0530

    feat: setup database session

---
+++ app/database/session.py
@@ -0,0 +1,10 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from app.core.config import settings
+
+engine = create_async_engine(settings.DATABASE_URL, echo=True)
+
+SessionLocal = async_sessionmaker(
+  bind=engine,
+  class_=AsyncSession,
+  expire_on_commit=False
+)
