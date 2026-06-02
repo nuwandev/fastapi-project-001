7ae11da22ec9b52065f8a624658dafab27a48fc8
Theekshana Nuwan <theekshanabro40@gmail.com>
2026-06-02 16:13:30 +0530

    refactor: inject user service into routes

--- app/api/routes/user_routes.py
+++ app/api/routes/user_routes.py
@@ -1,47 +1,29 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.database.session import get_db
 from app.repositories.user_repository import UserRepository
 from app.services.user_service import UserService
 from app.schemas.user_schema import UserCreate, UserResponse
+from app.dependencies.user import get_user_repository, get_user_service
 
 router = APIRouter(prefix="/users", tags=["users"])
 
 @router.post("/", response_model=UserResponse)
 async def create_user(
   user: UserCreate,
-  db: AsyncSession = Depends(get_db)
+  service: UserService = Depends(get_user_service),
 ):
-  repo = UserRepository(db)
-  service = UserService(repo)
-  
-  crated_user = await service.create_user(
-    name=user.name,
-    email=user.email
-  )
-
-  return crated_user
+  return await service.create_user(user)
 
 @router.get("/{user_id}", response_model=UserResponse)
 async def get_user(
   user_id: int,
-  db: AsyncSession = Depends(get_db)
+  service: UserService = Depends(get_user_service)
 ):
-  repo = UserRepository(db)
-  service = UserService(repo)
-  
-  user = await service.get_user_by_id(user_id)
-
-  return user
+  return await service.get_user_by_id(user_id)
 
 @router.get("/", response_model=list[UserResponse])
 async def get_users(
-  db: AsyncSession = Depends(get_db)
+  service: UserService = Depends(get_user_service)
 ):
-  repo = UserRepository(db)
-  service = UserService(repo)
-  
-  users = await service.get_users()
-
-  return users
+  return await service.get_users()
