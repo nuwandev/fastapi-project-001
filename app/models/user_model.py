65153e186f675f1cf06c112a0e3bb711b25e82c4
Theekshana Nuwan <theekshanabro40@gmail.com>
2026-06-02 14:44:26 +0530

    refactor: centralize declarative Base for models

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,9 +1,6 @@
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import String, Integer
-from datetime import datetime
-
-class Base(DeclarativeBase):
-    pass
+from app.database.base import Base
 
 class User(Base):
     __tablename__ = "users"
